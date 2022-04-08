ceb8f17c7f382c4ce1bde0d0f8d25b21f7b3cf9f
Anubis <pkhaan@protonmail.com>
2022-04-08 03:00:21 +0300

    Zookeeper init

---
+++ NaniApp/app/src/main/java/gr/aueb/distributedsystems/naniapp/skeletonBackend/Node.java
@@ -0,0 +1,74 @@
+package gr.aueb.distributedsystems.naniapp.skeletonBackend;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+The Base Node Class for Implementation of dataNode
+and of course for the storage of Broker and the Publisher 
+nad Customer linked through the Zookeeper Class*/
+public class Node implements Serializable {
+
+ //Broker Addresses (ip, port)
+    static public final ArrayList<Address> BROKER_ADDRESSES = new ArrayList<>(Arrays.asList(
+            new Address("192.168.1.4", 6000),
+            new Address("192.168.1.4", 7000),
+            new Address("192.168.1.4", 8000)));
+
+
+
+             //DataNode Addresses (ip, port)
+    static public final ArrayList<Address> DATANODE_ADDRESSES = new ArrayList<>(Arrays.asList(
+            new Address("127.0.0.1", 7000),
+            new Address("127.0.0.1", 7250),
+            new Address("127.0.0.1", 7500),
+            new Address("127.0.0.1", 7750)));
+
+ //Zookeeper Address (ip, port)
+    static public final Address ZOOKEEPER_ADDRESS = new Address("192.168.1.4", 10000);
+
+    //backlog
+    static public final int BACKLOG = 250;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ NaniApp/app/src/main/java/gr/aueb/distributedsystems/naniapp/skeletonBackend/Zookeeper.java
@@ -0,0 +1,157 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+/*
+ZooKeeper is used in distributed systems for service synchronization and as a naming registry.
+ When working with Apache Kafka, ZooKeeper is primarily used to track the status of nodes in
+  the Kafka cluster and maintain a list of Kafka topics and messages.
+*/
+//##############################################################################################
+/*
+It is a project of the Apache Software Foundation.
+ ZooKeeper is essentially a service for distributed 
+ systems offering a hierarchical key-value store,
+  which is used to provide a distributed configuration
+  service, synchronization service, and naming registry 
+  for large distributed systems (see Use cases).
+*/
+
+/*So our zookeeper will be tasked as to update
+and keep track of any event that takes place in
+a our current Data Keeper*/
+
+public class Zookeeper extends Node{
+
+ServerSocket zkServerSocket = null;
+Address zkAddr = Node.ZK_ADDRESS;
+transient DataKeeper dataKeeper;
+
+
+//Initially we have the constructor of the Zookeeper
+public Zookeeper(){
+        //create new InfoTable obj for the system
+        dataKeeper = new DataKeeper();
+        System.out.println("{ZK_(All)}\\:Booted..........." + zkAddr.toString());
+    }
+
+/*In here a new server socket for the zookeeper 
+is created and awaits any broker requests.
+Whilst the signal is given green the zookeeper proceeds
+to run the new thread as per broker request*/
+
+public void initZKSocket(){
+
+
+try{
+    zkServerSocket = new ServerSocket(zkAddr.getPort(), Node.BACKLOG);
+    Socket brockerSocket;
+    System.out.println("{ZK_(requesting())}\\:Ready to accept new requests..........");
+}
+while (true) {
+                brockerSocket = zkServerSocket.accept();
+                Thread brokerThread = new ZooToBrokerConnection(brockerSocket, this);
+                brokerThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                zookeeperServerSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

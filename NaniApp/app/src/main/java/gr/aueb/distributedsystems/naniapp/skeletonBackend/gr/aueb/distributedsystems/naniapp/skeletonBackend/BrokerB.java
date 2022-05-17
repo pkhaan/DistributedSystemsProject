e68a7fff9e50ab7578f0e3c662802922fb13f5ce
pkhaan <pkhaan@pm.me>
2022-05-17 14:18:56 +0300

    Deleting Zookeeper Branch
    Deletion of Zookeeper Attempt in order to merge the
    working branch

--- NaniApp/app/src/main/java/gr/aueb/distributedsystems/naniapp/skeletonBackend/gr/aueb/distributedsystems/naniapp/skeletonBackend/BrokerB.java
+++
@@ -1,8 +0,0 @@
-
-package gr.aueb.distributedsystems.naniapp.skeletonBackend;
-
-public class BrokerB {
-    public static void main(String[] args) {
-        new Broker(Node.BROKER_ADDRESSES.get(1)).init();
-    }
-}
--- NaniApp/app/src/main/java/gr/aueb/distributedsystems/naniapp/skeletonBackend/gr/aueb/distributedsystems/naniapp/skeletonBackend/BrokerC.java
+++
@@ -1,9 +0,0 @@
-
-package gr.aueb.distributedsystems.naniapp.skeletonBackend;
-public class BrokerC {
-    public static void main(String[] args) {
-        new Broker(Node.BROKER_ADDRESSES.get(2)).init();
-    }
-}
-
-
